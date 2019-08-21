d2a7c03beeca0532f14e9e508381677730b97609
iLLiCiTiT <jakub.trllo@gmail.com>
2019-08-21 14:01:43 +0200

    created asset delegate which is able to paint colored rectangles under name

--- avalon/tools/gui/delegates/__init__.py
+++ avalon/tools/gui/delegates/__init__.py
@@ -2,10 +2,12 @@ from . import lib
 
 from .delegate_pretty_time import PrettyTimeDelegate
 from .delegate_version import VersionDelegate
+from .delegate_asset import AssetDelegate
 
 __all__ = [
     "lib",
 
     "PrettyTimeDelegate",
-    "VersionDelegate"
+    "VersionDelegate",
+    "AssetDelegate"
 ]
