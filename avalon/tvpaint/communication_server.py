b8f168fd6615fade454b48866e9c6955e05055ef
iLLiCiTiT <jakub.trllo@gmail.com>
2021-10-08 18:14:31 +0200

    implemented QtCommunicator which will contain qt secific implementation

--- avalon/tvpaint/communication_server.py
+++ avalon/tvpaint/communication_server.py
@@ -1013,3 +1013,40 @@ class Communicator:
         log.info("Stopping communication")
         self.websocket_server.stop()
         self.qt_app.quit()
+
+
+class QtCommunicator(Communicator):
+    def __init__(self, qt_app):
+        super().__init__()
+        self.callback_queue = Queue()
+        self.qt_app = qt_app
+
+    def execute_in_main_thread(self, main_thread_item, wait=True):
+        """Add `MainThreadItem` to callback queue and wait for result."""
+        self.callback_queue.put(main_thread_item)
+        if wait:
+            return main_thread_item.wait()
+        return
+
+    async def async_execute_in_main_thread(self, main_thread_item, wait=True):
+        """Add `MainThreadItem` to callback queue and wait for result."""
+        self.callback_queue.put(main_thread_item)
+        if wait:
+            return await main_thread_item.async_wait()
+
+    def main_thread_listen(self):
+        """Get last `MainThreadItem` from queue.
+
+        Must be called from main thread.
+
+        Method checks if host process is still running as it may cause
+        issues if not.
+        """
+        # check if host still running
+        if self.process.poll() is not None:
+            self._exit()
+            return None
+
+        if self.callback_queue.empty():
+            return None
+        return self.callback_queue.get()
