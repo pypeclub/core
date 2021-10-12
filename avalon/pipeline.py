f80bbbe3a50b75bd4b1e32fe668dedc93662c64e
Jakub Jezek <jakub@orbi.tools>
2021-10-12 16:40:43 +0200

    global: add representation class method to loader and discovery

--- avalon/pipeline.py
+++ avalon/pipeline.py
@@ -197,6 +197,10 @@ class Loader(list):
     def __init__(self, context):
         self.fname = self.filepath_from_context(context)
 
+    @classmethod
+    def get_representations(cls):
+        return cls.representations
+
     def filepath_from_context(self, context):
         return get_representation_path_from_context(context)
 
@@ -1967,8 +1971,9 @@ def is_compatible_loader(Loader, context):
     representation = context["representation"]
     has_family = ("*" in Loader.families or
                   any(family in Loader.families for family in families))
-    has_representation = ("*" in Loader.representations or
-                          representation["name"] in Loader.representations)
+    representations = Loader.get_representations()
+    has_representation = ("*" in representations or
+                          representation["name"] in representations)
     return has_family and has_representation
 
 
